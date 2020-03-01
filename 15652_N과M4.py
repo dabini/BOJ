9f00d4e94e89b57f92346baffa019ebd972223ca
dabin Jo <jdb960211@gmail.com>
2020-03-01 21:53:45 +0900

    3월 1일

---
+++ 15652_N과M4.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+a = [0] * m
+ans = []
+def backtrack(index, n, m):
+    if index == m:
+        res = []
+        for i in range(m):
+            res.append(a[i])
+        if res[::-1] not in ans:
+            ans.append(res)
+    else:
+        for i in range(1, n + 1):
+            a[index] = i
+            backtrack(index + 1, n, m)
+
+backtrack(0, n, m)
+for a in ans:
+    print(*a)
