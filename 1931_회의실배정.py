270eb3d96b8644ed21cd335008a258d13ec04a20
dabin Jo <jdb960211@gmail.com>
2020-03-20 15:10:47 +0900

    3월 20일

--- 1931_회의실배정.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-lst = []
-for _ in range(N):
-    start, end = map(int, input().split())
-    lst.append((start, end))
-for n in range(N):
-    max_num = 0
-    for m in range(n+1, M):
