3d0893a8f4a03eb67edb2da0c9d5a23e79ac3383
dabin Jo <jdb960211@gmail.com>
2020-04-29 08:07:43 +0900

    4월 29일

--- 1991_트리순회.py
+++ 1991_트리순회.py
@@ -1,19 +1,38 @@
 import sys
+input = sys.stdin.readline
+
 def preorder_traverse(T):
     if T:
-        print("%d" %T, end=' ')
+        # res.add(T)
+        print(chr(T + ord('A')-1), end='')
+        preorder_traverse(tree[T][0])
         preorder_traverse(tree[T][1])
-        preorder_traverse(tree[T][2])
+
+def inorder_traverse(T):
+    if T:
+        inorder_traverse(tree[T][0])
+        print(chr(T + ord('A')-1), end='')
+        inorder_traverse(tree[T][1])
+
+def outorder_traverse(T):
+    if T:
+        outorder_traverse(tree[T][0])
+        outorder_traverse(tree[T][1])
+        print(chr(T + ord('A')-1), end='')
 
 
 input = sys.stdin.readline
 N = int(input())
-tree = [[0]*2 for _ in range(N)]
-for n in range(N):
+tree = [[0]*2 for _ in range(N+1)]
+for n in range(1, N+1):
     root, left, right = map(str, input().split())
     if left != '.':
-        tree[n][0] =ord(left) - ord('A')
+        tree[ord(root) - ord('A') + 1][0] =ord(left) - ord('A') + 1
     if right != '.':
-        tree[n][1] = ord(right) - ord('A')
-print(tree)
-# preorder_traverse(0)
+        tree[ord(root) - ord('A') + 1][1] = ord(right) - ord('A') + 1
+# print(tree)
+preorder_traverse(1)
+print()
+inorder_traverse(1)
+print()
+outorder_traverse(1)
