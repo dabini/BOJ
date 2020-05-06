d3a63c530d8e360ad28cfa9df41a98d10884cfb7
dabini <jdb960211@gmail.com>
2020-05-06 15:26:03 +0900

    5월 6일

--- 1700_멀티탭스케줄링.py
+++ 1700_멀티탭스케줄링.py
@@ -1,12 +1,5 @@
 import sys
 
+input = sys.stdin.readline
 N, K = map(int, input().split())
-plugs = dict()
-for i in range(1, K+1):
-    plugs[i] = 0
 lst = list(map(int, input().split()))
-for l in lst:
-    plugs[l] += 1
-# print(plugs)
-
-check = [0 for _ in range(N)]
