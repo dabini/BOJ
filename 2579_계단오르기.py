0d17ec31db18d78a9cba3b6152282bdd0dba4a74
dabini <jdb960211@gmail.com>
2020-05-24 00:24:40 +0900

    dp

--- 2579_계단오르기.py
+++ 2579_계단오르기.py
@@ -1,20 +1,23 @@
-stairs_num = int(input())
-stair_lst = []
-for n in range(stairs_num):
-    stair = int(input())
-    stair_lst += [stair]
-howmany = len(stair_lst)
-ans = 0
-def Getsome(here):
-    global ans, howmany
+import sys
 
-    if here > howmany:
-        return
-    elif here == howmany:
-        return
+
+input = sys.stdin.readline
+n = int(input())
+stair_lst = [0]
+for n in range(n):
+    stair_lst.append(int(input()))
+
+score = [[0, 0] for _ in range(n+2)]
+
+for i in range(1, n+2):
+    if i == 1:
+        score[1][0] = stair_lst[1]
+    elif i ==2:
+        score[2][0] = stair_lst[1] + stair_lst[2]
     else:
-        ans += stair[here]
-        return Getsome(here+1)
-        return Getsome(here+2)
-Getsome(0)
-print(ans)
+        score[i][0] = max(score[i-3]) + stair_lst[i-1] + stair_lst[i]
+        score[i][1] = max(score[i-2]) + stair_lst[i]
+# print(stair_lst)
+# print(score)
+print(max(score[n+1]))
+
