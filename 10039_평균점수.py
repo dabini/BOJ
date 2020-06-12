1e00904bbf4dbeeb1fd930ba52ee0376c3fd1b1f
dabini <jdb960211@gmail.com>
2020-06-12 23:28:22 +0900

    10039

---
+++ 10039_평균점수.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+
+total = 0
+for _ in range(5):
+    score = int(input())
+    if score <= 40:
+        total += 40
+    else:
+        total += score
+print(total//5)
