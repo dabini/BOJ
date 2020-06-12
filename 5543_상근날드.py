ee97798d4dcc2b493dc82d52e509b19117ecda46
dabini <jdb960211@gmail.com>
2020-06-12 23:33:38 +0900

    상근날드

---
+++ 5543_상근날드.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+burger = []
+beverage = []
+for _ in range(3):
+    burger.append(int(input()))
+for _ in range(2):
+    beverage.append(int(input()))
+
+print(min(burger) + min(beverage) - 50)
