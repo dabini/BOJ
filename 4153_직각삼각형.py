b52c42b9b7f90f29a59029fb26bb2d70431bb635
dabini <jdb960211@gmail.com>
2020-07-14 23:18:49 +0900

    피타고라스

---
+++ 4153_직각삼각형.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+while True:
+    num_list = list(map(int, input().split()))
+    if num_list == [0, 0, 0]:
+        break
+    num_list.sort()
+    if num_list[2] **2 == num_list[0] **2 + num_list[1]**2:
+        print("right")
+    else:
+        print("wrong")
