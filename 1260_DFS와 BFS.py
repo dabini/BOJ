79dafb634f6b8ea22009df8d2ba4e2d18cb45cf1
dabini <jdb960211@gmail.com>
2020-07-22 17:24:48 +0900

    change

--- 1260_DFS와 BFS.py
+++ 1260_DFS와 BFS.py
@@ -1,30 +1,30 @@
-def DFS(V):
-    global visited
-    visited.append(V)
-    for i in range(N+1):
-        if field[V][i] == 1 and i not in visited:
-            visited = DFS(i)
-    return visited
+def DFS(st):
+    visited.append(st)
+    for now in range(1, N+1):
+        if arr[st][now] and now not in visited:
+            DFS(now)
 
-def BFS(V):
-    q = [V]
-    res = [V]
+def BFS(st):
+    q = [st]
+    res = [st]
     while q:
         now = q.pop(0)
         for new in range(N+1):
-            if field[now][new] == 1 and new not in res:
+            if arr[now][new] and new not in res:
                 res.append(new)
                 q.append(new)
     return res
 
-
-N, M, V = map(int, input().split())
-field = [[0] * (N+1) for _ in range(N+1)]
+N, V, num = map(int, input().split())
+arr = [[0]*(N+1) for _ in range(N+1)]
 visited = []
-for m in range(M):
-     a, b = map(int, input().split())
-     field[a][b] = 1
-     field[b][a] = 1
-DFS(V)
+for _ in range(V):
+    s, e = map(int, input().split())
+    arr[s][e] = 1
+    arr[e][s] = 1
+
+# print(arr)
+DFS(num)
 print(*visited)
-print(*BFS(V))
+print(*BFS(num))
+
